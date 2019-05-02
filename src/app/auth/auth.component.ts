a35bf3558eba78d8489f25cd5ea590799619bebc
Prabeen <prabeensoreng2119@gmail.com>
2019-05-02 19:36:56 +0530

    structured auth component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import * as M from "materialize-css";
+
+@Component({
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"]
+})
+export class AuthComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    const tabs = document.querySelector(".tabs");
+    M.Tabs.init(tabs, {});
+  }
+}
