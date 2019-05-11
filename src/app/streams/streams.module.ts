a08fdf285db74103ab6556ba823ce91b29cb8f61
Prabeen <prabeensoreng2119@gmail.com>
2019-05-11 10:33:03 +0530

    side and post components are added

--- src/app/streams/streams.module.ts
+++ src/app/streams/streams.module.ts
@@ -6,9 +6,12 @@ import { TokenService } from "../services/token.service";
 import { StreamsRoutingModule } from "./streams-routing.module";
 import { StreamsComponent } from "./streams.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { SideComponent } from './side/side.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [StreamsComponent, NavbarComponent],
+  declarations: [StreamsComponent, NavbarComponent, SideComponent, PostFormComponent, PostsComponent],
   imports: [
     CommonModule,
     FormsModule,
